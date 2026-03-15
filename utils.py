fdbdcbba89fba1715f0c03a3fec7a7d8653264b3
Daniel Alm D'Adda <swedmarine@hotmail.com>
2026-03-15 14:34:15 +0100

    Unified plst and fnm set info retrieval

--- utils.py
+++ utils.py
@@ -6,15 +6,15 @@ PLST_SOURCE = Path('plst_cardlist.csv')
 FNM_SOURCE = Path('fnm_cardlist.csv')
 
 
-def get_plst_cardnumber(cardname: str, cardnumber: str) -> str:
+def get_plst_set_info(cardname: str, cardnumber: str) -> tuple[str, str]:
     with PLST_SOURCE.open('r', encoding = 'utf-8') as file:
         reader = csv.DictReader(file)
         for line in reader:
             if line['Name'] == cardname and cardnumber in line['CN']:
-                return line['CN']
+                return line['Set'], line['CN']
 
 
-def get_fnm_setcode_number(cardname: str) -> tuple[str, str]:
+def get_fnm_set_info(cardname: str) -> tuple[str, str]:
     with FNM_SOURCE.open('r', encoding = 'utf-8') as file:
         reader = csv.DictReader(file)
         for line in reader:
@@ -39,10 +39,11 @@ def save_deckbox_to_moxfield(save_path: Path, card_dicts: list[dict[str, str]]) 
         for entry in card_dicts:
             entry['Count'] = entry['Tradelist Count']
             if entry['Edition'] == 'The List':
-                entry['Edition Code'] = 'PLST'
-                entry['Card Number'] = get_plst_cardnumber(entry['Name'], entry['Card Number'])
+                edition_code, cardnumber = get_plst_set_info(entry['Name'], entry['Card Number'])
+                entry['Edition Code'] = edition_code
+                entry['Card Number'] = cardnumber
             elif entry['Edition'] == 'Friday Night Magic':
-                edition_code, cardnumber = get_fnm_setcode_number(entry['Name'])
+                edition_code, cardnumber = get_fnm_set_info(entry['Name'])
                 entry['Edition Code'] = edition_code
                 entry['Card Number'] = cardnumber
             entry = {key: entry[key] for key in fieldnames}
