007230322a6358d8d93fba2b2f493f1b8c7871bc
Daniel Alm D'Adda <swedmarine@hotmail.com>
2026-04-19 17:32:48 +0200

    Added option for exact matches. Removed error when adding trades with tradelist count 0.

--- trade_matcher.py
+++ trade_matcher.py
@@ -1,40 +1,35 @@
-import csv
 from pathlib import Path
 
-from utils import save_deckbox_to_moxfield, verify_path
+from utils import (
+    read_deckbox,
+    read_wishlist,
+    save_deckbox_to_moxfield,
+    verify_path
+)
 
 
-def match_trade(wishlist_path: Path|str, tradelist_path: Path|str, save_path: Path|str) -> None:
+def match_trade(wishlist_path: Path|str, tradelist_path: Path|str, save_path: Path|str, exact_match: bool = False) -> None:
     wishlist_path = verify_path(wishlist_path, verify_exists = True)
     tradelist_path = verify_path(tradelist_path, verify_exists = True)
     save_path = verify_path(save_path, verify_available = True)
     
-    wishlist: list[dict[str, int|str]] = []
-    with wishlist_path.open('r', encoding='utf-8') as file:
-        for line in file.readlines():
-            split_line = line.split()
-            if '*' in split_line[-1]:
-                split_line.pop()
-            
-            count, *name = split_line[:-2]
-            name = ' '.join(name)
-            card = {'Count': count, 'Name': name}
-            wishlist.append(card)
-
-    with tradelist_path.open('r', encoding='utf-8') as file:
-        reader = csv.DictReader(file)
-        tradelist = [line for line in reader]
-    
-    trade_gen = lambda name : (card for card in tradelist
-                               if card['Name'] == name)
-    
     trades = []
-    for wish in wishlist:
-        availables = list(trade_gen(wish['Name']))
-        if availables:
-            total_count = sum([int(trade['Tradelist Count']) for trade in availables])
+    wishlist_gen = read_wishlist(wishlist_path)
+    for wish in iter(wishlist_gen):
+        if exact_match:
+            trade_gen = read_deckbox(tradelist_path, wish['Name'], wish['Edition Code'], wish['Card Number'])
+        else:
+            trade_gen = read_deckbox(tradelist_path, wish['Name'])
+        
+        total_count = 0
+        for trade in iter(trade_gen):
+            trade_count = int(trade['Tradelist Count'])
+            if trade_count > 0:
+                total_count += int(trade['Tradelist Count'])
+                trades.append(trade)
+        
+        if total_count > 0:
             print(wish['Name'], wish['Count'], '/', total_count)
-        trades.extend(availables)
     
     save_deckbox_to_moxfield(save_path, trades)
 
